f3d42e41ba59a537b1ffebbe0357225d8584e55c
Ashok Pundir <pundiramit936@gmail.com>
2018-04-16 22:05:08 -0700

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,32 +1,16 @@
-<--?php 
-if(isset($_GET['submit'])){
-    $to ="pundiramit936@gmail.com";
-    $from =$_GET['email'];
-    $first_name =$_GET['fname'];
-    $last_name =$_GET['lname'];
-    $subject ="Contact Form";
-    $message =$first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_GET['textarea'];
-   
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    }
-?-->
+
  <?php
 
-
-
-
-
-
+ $name=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$text=$_POST['textarea'];
          $to ="pundiramit936@gmail.com";
          $subject = "This is subject";
          
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
+         $message = "<b>$name</b>";
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header = "From:$email \r\n";
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          
